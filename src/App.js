b2d56b08f8e1e1bb3c49f0a245a45e5acc18d77f
James Lewis <james.lewis@autotrader.com>
2017-08-06 20:24:22 -0400

    add test video and player

--- src/App.js
+++ src/App.js
@@ -1,19 +1,63 @@
 import React, { Component } from 'react';
-import hat from './hat.png';
+import { Player, ControlBar } from 'video-react';
+import video from './video.mp4';
+import classnames from 'classnames';
+import "video-react/dist/video-react.css";
 import './App.css';
 
+
+
 class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            showPrescreen: true,
+        }
+
+        this.handlePrescreenClick = this.handlePrescreenClick.bind(this);
+    }
+
+    handlePrescreenClick() {
+        this.setState({
+            showPrescreen: false,
+        }, () => {
+            this.refs.player.play();
+        });
+    }
+
+    renderVideo() {
+        return (
+            <Player
+                ref="player"
+                preload="auto"
+                playsInline
+                src={video}
+            >
+                <ControlBar autoHide/>
+            </Player>
+        );
+    }
+
+    renderPrescreen() {
+        const componentClasses = classnames({
+            prescreen: true, 
+            hidden: !this.state.showPrescreen,
+        });
+
+        return (
+            <div className={componentClasses}
+                onClick={this.handlePrescreenClick}
+            />
+        );
+    }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={hat} className="App-logo" alt="logo" />
-          <h2>Welcome to the Story Hat</h2>
+        <div className="app-container">
+            {this.renderVideo()}
+            {this.renderPrescreen()}
         </div>
-        <p className="App-intro">
-          Soon this is going to be awesome.
-        </p>
-      </div>
     );
   }
 }
